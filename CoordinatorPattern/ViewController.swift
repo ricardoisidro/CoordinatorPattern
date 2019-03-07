234692ae0f4ce56bee98394217e0ac688363d7da
ricardo <ricardo.isidro.86@gmail.com>
2019-03-06 23:18:55 -0600

    Initial Commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Ricardo Isidro on 3/6/19.
+//  Copyright © 2019 ricardoIsidro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
